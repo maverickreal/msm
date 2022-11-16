3351e95921a49c08aab29ba15867cda5d5bfcd5e
Aditya Saxena <adity09@outlook.com>
2022-11-16 15:46:09 +0530

    base complete

--- db.js
+++ db.js
@@ -17,43 +17,55 @@ const init = () => {
 }
 
 const verifyUser = id => {
-  let returnValue = 0;
-  db.query(`SELECT 1 FROM USERS WHERE 'USERID'='${id}' LIMIT 1;`, (err, res) => {
+  let error, returnValue;
+  db.query(`SELECT COUNT(*) FROM USERS WHERE 'USERID'='${id}';`, (err, res) => {
     if (err) {
-      console.log('error : ' + err);
+      error = err;
+    }
+    else if (res.rows.empty()) {
+      error = 'user not found';
     }
     else {
-      returnValue = !res.rows.empty() && res.rows[0] == '1';
+      returnValue = res.rows[0];
     }
   });
-  return returnValue;
+  return { error, returnValue };
 }
 
 const createUser = (name, email, password) => {
-  db.query(`INSERT INTO USERS(NAME, EMAIL PASSWORD) VALUES(${name}, ${email}, ${password});`, (err, res) => {
+  let error, user;
+  db.query(`INSERT INTO USERS('NAME', 'EMAIL', 'PASSWORD') VALUES('${name}', '${email}', '${password}');`, (err, res) => {
     if (err) {
-      console.log('error : ' + err);
+      error = err;
+    }
+    else if (res.rows.empty()) {
+      err = 'user could not be created';
+    }
+    else {
+      user = res.rows[0];
     }
   });
+  return { error, user };
 }
 
 const followUser = (followerId, followeeId) => {
   let error;
-  db.query(`SELECT 1 FROM USERS WHERE ANY(FOLLOWS)=${followeeId} LIMIT 1;`, err => error = err);
-  if (error) {
-    return error;
+  db.query(`SELECT COUNT(*) FROM USERS WHERE ANY('FOLLOWS')='${followeeId}';`, err => error = err);
+  if (!error) {
+    db.query(`UPDATE TABLE USERS SET 'FOLLOWS'[CARDINALITY('FOLLOWS')]='${followeeId}' WHERE 'ID'='${followerId}' LIMIT 1;`, err => error = err);
   }
-  db.query(`UPDATE TABLE USERS SET FOLLOWS[CARDINALITY(FOLLOWS)]=${followeeId} WHERE ID=${followerId} LIMIT 1;`, err => error = err);
   return error;
 }
 
 const unFollowUser = (followerId, followeeId) => {
-  db.query(`UPDATE TABLE USERS FOLLOWS=ARRAY_REMOVE(FOLLOWS, ${followeeId});`);
+  let error;
+  db.query(`UPDATE TABLE USERS SET 'FOLLOWS'=ARRAY_REMOVE('FOLLOWS', '${followeeId}') WHERE 'USERID'='${followerId}';`, err => error = err);
+  return error;
 }
 
 const getUserProfile = (userId) => {
-  let error, returnValue;
-  db.query(`SELECT NAME, EMAIL, CARDINALITY(FOLLOWS) FOLLOWCOUNT, CARDINALITY(POSTS) POSTCOUNT FROM USERS WHERE ID=${userId} LIMIT 1;`, (err, res) => {
+  let error, profile;
+  db.query(`SELECT 'NAME', 'EMAIL', CARDINALITY('FOLLOWS') FOLLOWCOUNT, CARDINALITY('POSTS') POSTCOUNT FROM USERS WHERE 'ID'='${userId}' LIMIT 1;`, (err, res) => {
     if (err) {
       error = err;
     }
@@ -61,12 +73,107 @@ const getUserProfile = (userId) => {
       error = 'user not found to exist';
     }
     else {
+      profile = res.rows[0];
+    }
+  });
+  return { error, profile };
+}
+
+const createPost = (title, desc, userId) => {
+  let error, post;
+  db.query(`INSERT INTO POSTS('TITLE', 'DESCRIPTION', 'USERID', 'CREATEDTIME') VALUES('${title}', '${desc}', '${userId}', CURENT_TIMESTAMP());`, (err, res) => {
+    if (err) {
+      error = err;
+    }
+    else if (res.rows.empty()) {
+      err = 'post could not be created';
+    }
+    else {
+      post = res.rows[0];
+    }
+  });
+  return { error, post };
+}
+
+const deletePost = (userId, postId) => {
+  let error;
+  db.query(`DELETE FROM POSTS WHERE 'ID'='${postId}' && 'USERID'='${userId}' LIMIT 1;`, err => error = err);
+  if (error) {
+    return error;
+  }
+  db.query(`UPDATE TABLE USERS SET 'POSTS'=ARRAY_REMOVE('POSTS', '${postId}');`, err => error = err);
+  if (error) {
+    return error;
+  }
+  db.query(`DELETE FROM COMMENTS WHERE 'POSTID'='${postId}';`, err => error = err);
+  return error;
+}
+
+const likePost = (userId, postId) => {
+  let error;
+  db.query(`UPDATE TABLE POSTS SET 'LIKES'=ARRAY_APPEND('LIKES', '${userId}') WHERE 'ID'='${postId}';`, err => error = err);
+  return error;
+}
+
+const unLikePost = (userId, postId) => {
+  let error;
+  db.query(`UPDATE TABLE POSTS SET 'LIKES'=ARRAY_REMOVE('LIKES', '${userId}') WHERE 'ID'='${postId}';`, err => error = err);
+  return error;
+}
+
+const createComment = (userId, postId, comment) => {
+  let error, returnValue;
+  db.query(`INSERT INTO COMMENTS('COMMENT', 'USERID', 'POSTID') VALUES('${comment}', '${userId}', '${postId}');`, (err, res) => {
+    if (err) {
+      error = err;
+    }
+    else if (res.rows.empty()) {
+      error = 'comment could not be created';
+    }
+    else {
       returnValue = res.rows[0];
     }
   });
-  return returnValue;
+  if (!error) {
+    db.query(`UPDATE TABLE POSTS SET 'COMMENTS'=ARRAY_APPEND('COMMENTS', '${returnValue.ID}') WHERE 'ID'='${postId}';`, err => error = err);
+  }
+  return { error, returnValue };
+}
+
+const getPost = postId => {
+  let error, post;
+  db.query(`SELECT * FROM POSTS WHERE 'ID'='${postId}' LIMIT 1;`, (err, res) => {
+    if (err) {
+      error = err;
+    }
+    else if (res.rows.empty()) {
+      error = 'post not found';
+    }
+    else {
+      post = res.rows[0];
+    }
+  });
+  return { error, post };
+}
+
+const getPostsOfUser = userId => {
+  let error, posts;
+  db.query(`SELECT * FROM POSTS WHERE 'ID' IN (
+    SELECT POSTS FROM USERS WHERE 'ID'='${userId}' LIMIT 1;
+    );`, (err, res) => {
+    if (err) {
+      error = err;
+    }
+    else if (res.rows.empty()) {
+      error = 'posts not found';
+    }
+    else {
+      posts = res.rows;
+    }
+  });
+  return { error, posts };
 }
 
 module.exports = {
-  init, verifyUser, createUser, followUser, unFollowUser, getUserProfile
+  init, verifyUser, createUser, followUser, unFollowUser, getUserProfile, createPost, deletePost, likePost, unLikePost, createComment, getPost, getPostsOfUser
 }
--- index.js
+++ index.js
@@ -3,75 +3,202 @@ const app = require('express')(), db = require('./db.js'), jwt = require('jsonwe
 db.init();
 
 app.post('/api/authenticate', (req, res) => {
-    if (db.verifyUser(userid)) {
-        return res.send({
-            status: 'error', message: 'already authenticated'
-        });
+    let [error, verified] = db.verifyUser(req.headers.userid);
+    if (error || !verified) {
+        returnValue = { status: 'error', message: 'already authenticated' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
     }
-    db.createUser(req.email, req.password);
-    const data = {
-        status: 'ok', email: req.email
+    const { user } = db.createUser(req.email, req.password);
+    let returnValue;
+    if (error) {
+        returnValue = { status: 'error', message: error };
     }
-    const token = jwt.sign(data, process.env.JWTSECRETKEY);
+    else {
+        returnValue = { status: 'ok', message: user };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
     res.send(token);
 });
 
 app.post('/api/follow/:followeeId', (req, res) => {
     const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
     if (!verified) {
-        return res.send({ status: 'error', message: 'verification failed' });
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
     }
-    const error = db.followUser(req.headers(userid), req.params['followeeId']);
+    let returnValue, error = db.followUser(req.headers.userid, req.params['followeeId']);
     if (error) {
-        res.send({ status: 'error', message: error });
+        returnValue = { status: 'error', message: error };
     }
     else {
-        res.send({ status: 'ok' });
+        returnValue = { status: 'ok' };
     }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
 app.post('/api/unfollow/:followeeId', (req, res) => {
     const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
     if (!verified) {
-        return res.send({ status: 'error', message: 'verification failed' });
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
     }
-    db.unFollowUser(req.headers(userid), req.params['followeeId']);
-    res.send({ status: 'ok' });
+    let returnValue, error = db.unFollowUser(req.headers.userid, req.params['followeeId']);
+    if (error) {
+        returnValue = { status: 'error', message: error };
+    }
+    else {
+        returnValue = { status: 'ok' };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
 app.get('/api/user', (req, res) => {
     const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
     if (!verified) {
-        return res.send({ status: 'error', message: 'verification failed' });
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
     }
-    const [profile, error] = db.getUserProfile(req.headers(userid));
+    let returnValue, [error, profile] = db.getUserProfile(req.headers.userid);
     if (error) {
-        res.send({ status: 'error', message: error });
+        returnValue = { status: 'error', message: error };
     }
     else {
-        res.send({ status: 'ok', profile });
+        returnValue = { status: 'ok', message: profile };
     }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
 app.post('/api/posts', (req, res) => {
+    const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
+    if (!verified) {
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
+    }
+    let returnValue, [post, error] = db.createPost(req.headers.userid, req.headers.title, req.headers.description);
+    if (error) {
+        returnValue = { status: 'error', message: error };
+    }
+    else {
+        returnValue = { status: 'ok', message: post };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
-app.delete('/api/posts/:id', (req, res) => {
+app.delete('/api/posts/:postId', (req, res) => {
+    const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
+    if (!verified) {
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
+    }
+    let returnValue, error = db.deletePost(req.headers.userid, req.params['postId']);
+    if (error) {
+        returnValue = { status: 'error', message: 'error' };
+    }
+    else {
+        returnValue = { status: 'ok' };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
-app.post('/api/like/:id', (req, res) => {
+app.post('/api/like/:postId', (req, res) => {
+    const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
+    if (!verified) {
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
+    }
+    let returnValue, error = db.likePost(req.headers.userid, req.params['postId']);
+    if (error) {
+        returnValue = { status: 'error', message: error };
+    }
+    else {
+        returnValue = { status: 'ok' };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
-app.post('/api/unlike/:id', (req, res) => {
+app.post('/api/unlike/:postId', (req, res) => {
+    const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
+    if (!verified) {
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
+    }
+    let returnValue, error = db.unLikePost(req.headers.userid, req.params['postId']);
+    if (error) {
+        returnValue = { status: 'error', message: error };
+    }
+    else {
+        returnValue = { status: 'ok' };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
-app.post('/api/comment/:id', (req, res) => {
+app.post('/api/comment/:postId', (req, res) => {
+    const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
+    if (!verified) {
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
+    }
+    let returnValue, [error, commentId] = db.createComment(req.headers.userid, req.params['postId'], req.headers.comment);
+    if (error) {
+        returnValue = { status: 'error', message: error };
+    }
+    else {
+        returnValue = { status: 'ok', message: commentId };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
-app.get('/api/post/:id', (req, res) => {
+app.get('/api/post/:postId', (req, res) => {
+    const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
+    if (!verified) {
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
+    }
+    let returnValue, [error, post] = db.getPost(req.params['postId']);
+    if (error) {
+        returnValue = { status: 'error', message: error };
+    }
+    else {
+        returnValue = { status: 'ok', message: post };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
 app.get('/api/all_posts', (req, res) => {
+    const jwtToken = req.header(process.env.JWTTOKENKEY), verified = jwt.verify(jwtToken, process.env.JWTSECRETKEY);
+    if (!verified) {
+        returnValue = { status: 'error', message: 'verification failed' };
+        const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+        return res.send(token);
+    }
+    let returnValue, [error, posts] = db.getPostsOfUser(req.headers.userid);
+    if (error) {
+        returnValue = { statsu: 'error', message: error };
+    }
+    else {
+        returnValue = { status: 'ok', message: posts };
+    }
+    const token = jwt.sign(returnValue, process.env.JWTSECRETKEY);
+    res.send(token);
 });
 
 app.listen(process.env.PORT);
